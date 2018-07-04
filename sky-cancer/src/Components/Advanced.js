a1f04433aaa95811f1e96feb361f8af9b1da75a9
annasla95 <annaslabuszewska@gmail.com>
2018-07-04 20:57:10 +0200

    # coloring buttons if clicked

--- sky-cancer/src/Components/Advanced.js
+++ sky-cancer/src/Components/Advanced.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import Links from "./Links";
+import LinksAdv from "./LinksAdv";
 
 const Advanced = () => (
 
     <div>
-        <Links />
+        <LinksAdv />
         <div className="input_panel">
             <form><br/>
             <input type="checkbox" className="marginL" /> Okoliczne lotniska<br/>
@@ -18,18 +18,22 @@ const Advanced = () => (
                                     <label htmlFor="from_input">Klasa podróży</label>
                                     <select id="from_input" value="Ekonomiczna">
                                         <option value="Ekonomiczna">Ekonomiczna</option>
+                                        <option value="Biznes">Biznes</option>
+                                        <option value="Dowolna">Dowolna</option>
                                     </select>
                                 </div>
 
                                 <div className="optionsBtn">
                                     <label htmlFor="to_input">Ilość osób</label>
-                                    <input id="to_input" type="text" value="Gdziekolwiek" />
+                                    <input id="to_input" type="text" value="1" />
                                 </div>
 
                                 <div className="optionsBtn">
                                     <label htmlFor="departure_input">Przesiadki</label>
                                     <select id="from_input" value="Długie">
                                         <option value="Długie">Długie</option>
+                                        <option value="Krótkie">Krótkie</option>
+                                        <option value="Dowolne">Dowolne</option>
                                     </select>
                                 </div>
 
@@ -37,6 +41,11 @@ const Advanced = () => (
                                     <label htmlFor="return_input">Linie lotnicze</label>
                                     <select id="from_input" value="Dowolne" className="marginB">
                                         <option value="Dowolne">Dowolne</option>
+                                        <option value="Ryanair">Ryanair</option>
+                                        <option value="Wizzair">Wizzair</option>
+                                        <option value="EasyJet">EasyJet</option>
+                                        <option value="Lufthansa">Lufthansa</option>
+                                        <option value="LOT">LOT</option>
                                     </select>
                                 </div>
                                 <div className="clear"></div>
